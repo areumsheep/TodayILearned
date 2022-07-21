4b8986aaebab18cb39875a1a33428387f22b6675
areumsheep <areumsheep@gmail.com>
2022-07-22 02:32:36 +0900

    ✨ feat: #1 Selector로 atom의 파생된 상태 만들기

--- react/recoil_test/src/components/TextInput.jsx
+++ react/recoil_test/src/components/TextInput.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { atom, useRecoilState } from 'recoil';
+import { atom, selector, useRecoilState, useRecoilValue } from 'recoil';
 
 const textState = atom({
   key: 'textState', // unique ID (with respect to other atoms/selectors)
   default: '', // default value (aka initial value)
 });
+const charCountState = selector({
+  key: 'charCountState', // unique ID (with respect to other atoms/selectors)
+  get: ({ get }) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
 
 const TextInput = () => {
+  const count = useRecoilValue(charCountState);
   const [text, setText] = useRecoilState(textState);
   const onChange = (event) => {
     setText(event.target.value);
@@ -17,6 +26,8 @@ const TextInput = () => {
       <input type='text' value={text} onChange={onChange} />
       <br />
       Echo: {text}
+      <br />
+      CharacterCount: {count}
     </div>
   );
 };
