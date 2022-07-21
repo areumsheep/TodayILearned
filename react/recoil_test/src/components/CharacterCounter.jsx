c5bd80ccb6262d7436cdc6b1e2bcf592ba455205
areumsheep <areumsheep@gmail.com>
2022-07-22 02:26:18 +0900

    ♻️ refactor: 폴더 구조 변경

--- react/recoil_test/src/components/CharacterCounter.jsx
+++ react/recoil_test/src/components/CharacterCounter.jsx
@@ -1,25 +1,6 @@
 import React from 'react';
-import { atom, useRecoilState } from 'recoil';
+import TextInput from './TextInput';
 
-const textState = atom({
-  key: 'textState', // unique ID (with respect to other atoms/selectors)
-  default: '', // default value (aka initial value)
-});
-
-const TextInput = () => {
-  const [text, setText] = useRecoilState(textState);
-  const onChange = (event) => {
-    setText(event.target.value);
-  };
-
-  return (
-    <div>
-      <input type='text' value={text} onChange={onChange} />
-      <br />
-      Echo: {text}
-    </div>
-  );
-};
 const CharacterCounter = () => {
   return (
     <div>
