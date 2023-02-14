94b8cb6448fa08da17437c6fdb6fc17f65fc9747
areumsheep <areumsheep@gmail.com>
2023-02-14 15:41:51 +0900

    💯알고리즘 - 백준 1766 문제집 재풀이

--- algorithm/1766.js
+++ algorithm/1766.js
@@ -11,113 +11,98 @@ rl.on('line', (line) => data.push(line)).on('close', () => {
   process.exit();
 });
 
-class Heap {
+class MinHeap {
   constructor() {
-    this.heap = []; // {key, value} 형태의 node들이 담긴다.
+    this.heap = [];
   }
 
-  getLeftChildIndex = (parentIndex) => parentIndex * 2 + 1;
-  getRightChildIndex = (parentIndex) => parentIndex * 2 + 2;
-  getParentIndex = (childrenIndex) => Math.floor((childrenIndex - 1) / 2);
-  heapifyUp = () => {
+  #getLeftChildIndex = (index) => index * 2 + 1;
+  #getRightChildIndex = (index) => index * 2 + 2;
+  #getParentIndex = (index) => Math.floor((index - 1) / 2);
+
+  #heapifyUp = () => {
     let index = this.heap.length - 1;
-    const lastInsertedNode = this.heap[index];
+    const lastNode = this.heap[index];
 
     while (index > 0) {
-      const parentIndex = this.getParentIndex(index);
+      const parentIndex = this.#getParentIndex(index);
 
-      if (this.heap[parentIndex].key > lastInsertedNode.key) {
-        this.heap[index] = this.heap[parentIndex];
-        index = parentIndex;
-      } else break;
+      if (this.heap[parentIndex] <= lastNode) break;
+      this.heap[index] = this.heap[parentIndex];
+      index = parentIndex;
     }
 
-    this.heap[index] = lastInsertedNode;
+    this.heap[index] = lastNode;
   };
-  heapifyDown = () => {
+  #heapifyDown = () => {
     let index = 0;
-    const count = this.heap.length;
     const rootNode = this.heap[index];
 
-    while (this.getLeftChildIndex(index) < count) {
-      const leftChildIndex = this.getLeftChildIndex(index);
-      const rightChildIndex = this.getRightChildIndex(index);
-
+    while (this.#getLeftChildIndex(index) < this.heap.length) {
+      const leftChildIndex = this.#getLeftChildIndex(index);
+      const rightChildIndex = this.#getRightChildIndex(index);
       const smallerChildIndex =
-        rightChildIndex < count &&
-        this.heap[rightChildIndex].key < this.heap[leftChildIndex].key
+        this.heap[rightChildIndex] < this.heap[leftChildIndex]
           ? rightChildIndex
           : leftChildIndex;
 
-      if (this.heap[smallerChildIndex].key <= rootNode.key) {
-        this.heap[index] = this.heap[smallerChildIndex];
-        index = smallerChildIndex;
-      } else break;
-
-      this.heap[index] = rootNode;
+      if (this.heap[smallerChildIndex] > rootNode) break;
+      this.heap[index] = this.heap[smallerChildIndex];
+      index = smallerChildIndex;
     }
+
+    this.heap[index] = rootNode;
   };
 
-  peek = () => this.heap[0];
-  insert = (key, value) => {
-    const node = { key, value };
-    this.heap.push(node);
-    this.heapifyUp(); // 배열 가장 끝에 node를 넣고 다시 min heap의 형태로 갖춘다.
+  isEmpty = () => this.heap.length <= 0;
+  insert = (value) => {
+    this.heap.push(value);
+    this.#heapifyUp();
   };
-  remove = () => {
-    const count = this.heap.length;
+  delete = () => {
+    const length = this.heap.length;
     const rootNode = this.heap[0];
 
-    if (count <= 0) return undefined;
-    if (count === 1) this.heap = [];
+    if (length <= 0) return null;
+    if (length === 1) this.heap = [];
     else {
       this.heap[0] = this.heap.pop();
-      this.heapifyDown();
+      this.#heapifyDown();
     }
 
     return rootNode;
   };
 }
 
-class PriorityQueue extends Heap {
-  constructor() {
-    super();
-  }
-
-  enqueue = (priority, value) => this.insert(priority, value);
-  dequeue = () => this.remove();
-  isEmpty = () => this.heap.length <= 0;
-}
-
 const solution = (data) => {
   const [count] = data.shift().split(' ').map(Number);
   const graph = Array.from({ length: count + 1 }, () => []);
   const inDegrees = Array.from({ length: count + 1 }, () => 0);
+  const minHeap = new MinHeap();
 
   for (const item of data) {
     const [to, from] = item.split(' ').map(Number);
-    inDegrees[from] += 1;
+    inDegrees[from]++;
     graph[to].push(from);
   }
 
-  const pq = new PriorityQueue();
   for (let i = 1; i < inDegrees.length; i++) {
     if (inDegrees[i] === 0) {
-      pq.enqueue(i, i);
+      minHeap.insert(i);
     }
   }
 
   const result = [];
-  while (!pq.isEmpty()) {
-    const { key } = pq.dequeue();
+  while (!minHeap.isEmpty()) {
+    const key = minHeap.delete();
     result.push(key);
+
     for (const item of graph[key]) {
       inDegrees[item]--;
       if (inDegrees[item] === 0) {
-        pq.enqueue(item, item);
+        minHeap.insert(item);
       }
     }
   }
-
   return result.join(' ');
 };
